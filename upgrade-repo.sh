36db4946a578bc2103d3b3f7b9700374275fd6bc
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 17:48:45 -0600

    System Generated from upgrade-repo.sh

--- upgrade-repo.sh
+++ upgrade-repo.sh
@@ -3,5 +3,6 @@ sudo cp -r /etc/nixos/* /htpc/nixos
 sudo cp -r /etc/nixos/* /dev-htpc/nixos
 
 git add *
+# change this to require developer input
 git commit -m "System Generated from upgrade-repo.sh"
 git push
