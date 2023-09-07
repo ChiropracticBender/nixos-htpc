f92b4d605d0433a7378fd578595f2417bec59a9e
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 20:34:18 -0600

    copy users hardware config to HTPC

--- tools/updater-clone-temp.sh
+++ tools/updater-clone-temp.sh
@@ -10,6 +10,8 @@ echo "Moving Files from downloaded repo to HTPC"
 sudo cp -r ./temp/* /htpc
 echo "Deleting temp files"
 sudo rm -r ./temp
+echo "Copying your hardware config to HTPC"
+sudo cp -r /etc/nixos/hardware-configuration.nix /htpc/nixos
 # NEEDS IF STATEMENT IS TRUE
 echo "Applying Updates to HTPC"
 sudo nixos-rebuild switch --flake /htpc/#NixOSHTPC
