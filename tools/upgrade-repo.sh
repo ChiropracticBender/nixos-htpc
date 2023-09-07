e969b3afd2a15da4c56bd49f283360152bea6c23
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 21:28:38 -0600

    System Generated from upgrade-repo.sh

--- tools/upgrade-repo.sh
+++ tools/upgrade-repo.sh
@@ -5,4 +5,14 @@ sudo cp -r /etc/nixos/* /dev-htpc/nixos
 git add *
 # change this to require developer input
 git commit -m "System Generated from upgrade-repo.sh"
+
+while true; do
+    read -p "Do you wish to publish changes to Github? " yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 git push
