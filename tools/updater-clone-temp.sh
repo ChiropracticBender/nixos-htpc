708d90b7a2f17a6b95c3e5d85cf94fb90fcc62f2
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 19:41:43 -0600

    Created repo clone tool for quick testing

--- tools/updater-clone-temp.sh
+++ tools/updater-clone-temp.sh
@@ -3,13 +3,13 @@
 echo "Entering NIX SHELL GIT"
 nix-shell -p git
 echo "Cloning MASTER Repo"
-git clone git@github.com:ChiropracticBender/nixos-htpc.git
+git clone git@github.com:ChiropracticBender/nixos-htpc.git temp
 echo "Making/Checking htpc dir"
 mkdir /htpc
 echo "Moving Files from downloaded repo to HTPC"
-cp -r ./nixos-htlp/* /htpc
+cp -r ./temp/nixos-htlp/* /htpc
 echo "Deleting temp files"
-rm -r nixos-htpc
+rm -r /temp/nixos-htpc
 # NEEDS IF STATEMENT IS TRUE
 echo "Applying Updates to HTPC"
 sudo nixos-rebuild switch --flake /htpc/#NixOSHTPC
