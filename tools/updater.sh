b03c18545afed5244945695809632b54a1a12dd4
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 19:30:57 -0600

    System Generated from upgrade-repo.sh

---
+++ tools/updater.sh
@@ -0,0 +1,15 @@
+## UPDATE SYSTEM
+
+echo "Checking for Updates"
+
+curl -L \
+  https://api.github.com/repos/chiropracticbender/nixos-htpc/releases/latest \
+| grep zipball_url \
+| grep -i -o "https://[^ ]*" \
+| wget -qi -
+
+# NEEDS IF STATEMENT IS TRUE
+echo "Applying Updates to HTPC"
+sudo nixos-rebuild switch --flake /htpc/#NixOSHTPC
+
+
