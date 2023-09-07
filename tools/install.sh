07017ad083ae104c4a9f567e1717697662f44ea3
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 22:59:02 -0600

    System Generated from upgrade-repo.sh

--- tools/install.sh
+++ tools/install.sh
@@ -9,9 +9,9 @@ sudo mkdir /htpc
 echo "Cloning MASTER Repo"
 sudo git clone https://github.com/ChiropracticBender/nixos-htpc tmp
 echo "Moving Files from downloaded repo to HTPC"
-sudo cp -r ./tmp/* /htpc
+sudo cp -r tmp/* /htpc
 echo "Deleting temp files"
-sudo rm -r ./tmp
+sudo rm -r tmp
 echo "Copying your hardware config to HTPC"
 sudo cp -r /etc/nixos/hardware-configuration.nix /htpc/nixos
 # NEEDS IF STATEMENT IS TRUE
