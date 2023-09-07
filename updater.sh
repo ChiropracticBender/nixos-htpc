0597110acf3860f228ece53b9f191b0a26d66bc3
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 19:19:58 -0600

    System Generated from upgrade-repo.sh

--- updater.sh
+++ updater.sh
@@ -2,13 +2,11 @@
 
 echo "Checking for Updates"
 
-curl -s https://api.github.com/repos/ChiropracticBender/nixos-htpc/latest \
-| grep browser_download_url \
-| cut -d : -f 2,3 \
-| tr -d \" \
+curl -L \
+  https://api.github.com/repos/chiropracticbender/nixos-htpc/releases/latest \
+| grep zipball_url \
+| grep -i -o "https://[^ ]*" \
 | wget -qi -
-
-echo $browser_download_url
 
 # NEEDS IF STATEMENT IS TRUE
 #echo "Applying Updates to HTPC"
