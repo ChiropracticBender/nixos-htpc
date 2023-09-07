cf39fab8a37d9c56471f529db31005566ee1cec8
ChiropracticBender <noreply@digiwizard.ca>
2023-09-06 19:54:23 -0600

    System Generated from upgrade-repo.sh

--- tools/updater-clone-temp.sh
+++ tools/updater-clone-temp.sh
@@ -3,13 +3,13 @@
 echo "Entering NIX SHELL GIT"
 #nix-shell -p git
 echo "Cloning MASTER Repo"
-git clone git@github.com:ChiropracticBender/nixos-htpc.git temp
+sudo git clone git@github.com:ChiropracticBender/nixos-htpc.git temp
 echo "Making/Checking htpc dir"
-mkdir /htpc
+sudo mkdir /htpc
 echo "Moving Files from downloaded repo to HTPC"
-cp -r ./temp/nixos-htlp/* /htpc
+sudo cp -r ./temp/nixos-htlp/* /htpc
 echo "Deleting temp files"
-rm -r /temp/nixos-htpc
+sudo rm -r /temp/nixos-htpc
 # NEEDS IF STATEMENT IS TRUE
 echo "Applying Updates to HTPC"
 sudo nixos-rebuild switch --flake /htpc/#NixOSHTPC
