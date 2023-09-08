40994028b6f108dc14523f750beed321a7ee4f28
ChiropracticBender <noreply@digiwizard.ca>
2023-09-08 14:45:59 -0600

    idk

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -93,8 +93,6 @@
     programs.bash.enable = true;
   };
 
-  home.stateVersion = "23.05"; # home manager state version
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
