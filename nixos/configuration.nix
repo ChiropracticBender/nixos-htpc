34fe609564b090b50dff68c9929be2a3aba81451
ChiropracticBender <noreply@digiwizard.ca>
2023-09-08 14:34:39 -0600

    changed structure of home manager code

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -86,7 +86,7 @@
       microsoft-edge
       kodi-wayland
     ];
-  home-manager.users.htpc = { pkgs, ... }: {
+  home-manager = { pkgs, ... }: {
     home.packages = [ pkgs.atool pkgs.httpie ];
     programs.kodi.datadir = "${config.xdg.dataHome}/htpc/kodi/htpc";
   };
