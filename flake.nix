effbec2fda2e6a6dad1aae55353b109fe28d93e8
ChiropracticBender <noreply@digiwizard.ca>
2023-09-08 15:14:40 -0600

    moved to flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
   
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs }:
@@ -24,6 +26,15 @@
 
       modules = [
       ./nixos/configuration.nix
+      home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.htpc = import ./nixos/home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
       ];
     };
   };
