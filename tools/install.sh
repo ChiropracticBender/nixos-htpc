a493a0e2581460428b9c1daf0b68f987436bea74
ChiropracticBender <noreply@digiwizard.ca>
2023-09-08 14:25:05 -0600

    ready for home manager config

--- tools/install.sh
+++ tools/install.sh
@@ -4,24 +4,32 @@ version="0.1"
 
 #echo "Cleaning HTPC dir"
 #sudo rm -r /htpc
-echo "Entering NIX SHELL GIT"
+#echo "Entering NIX SHELL GIT"
 #nix-shell -p git
+
 echo  "Adding home manger channel"
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 sudo nix-channel --update
+
 echo  "Making/Checking htpc dir"
 sudo mkdir /htpc
 sudo mkdir /htpc-tools
+
 echo "Cloning MASTER Repo"
 sudo git clone https://github.com/ChiropracticBender/nixos-htpc tmp
+
 echo "Moving Files from downloaded repo to HTPC"
 sudo cp -r tmp/* /htpc
+
 echo "Copying installer tool to HTPC-Tools"
 sudo cp -r tmp/tools/install.sh /htpc-tools/
+
 echo "Deleting temp files"
 sudo rm -r tmp
+
 echo "Copying your hardware config to HTPC"
 sudo cp -r /etc/nixos/hardware-configuration.nix /htpc/nixos
+
 # NEEDS IF STATEMENT IS TRUE
 echo "Applying Updates to HTPC"
 sudo nixos-rebuild switch --flake /htpc/#NixOSHTPC --show-trace --impure
