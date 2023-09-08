5287fb4dac7ef49689a90c01cc792a48b366692d
ChiropracticBender <noreply@digiwizard.ca>
2023-09-08 15:22:31 -0600

    default flake

--- flake.nix
+++ flake.nix
@@ -1,43 +1,29 @@
 {
-  description = "NixOS HTPC Developer Branch";
-  
+  description = "NixOS HTPC configuration";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";    
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
-    };
-
-  in 
-  {
-  
-  nixosConfigurations = {
-    NixOSHTPC = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit system; };
-
-      modules = [
-      ./nixos/configuration.nix
-      home-manager.nixosModules.home-manager
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.htpc = import ./nixos/home.nix;
+            home-manager.users.jdoe = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
-      ];
+        ];
+      };
     };
-  };
-
   };
 }
