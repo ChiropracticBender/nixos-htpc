9147bdc5fd7350ee2d823c2d43c062f0313800a8
ChiropracticBender <noreply@digiwizard.ca>
2023-09-09 13:46:09 -0600

    removed hardwareconfig and syncthing

--- flake.nix
+++ flake.nix
@@ -1,29 +1,34 @@
 {
-  description = "NixOS HTPC configuration";
-
+  description = "NixOS HTPC Developer Branch";
+  
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    # home-manager.url = "github:nix-community/home-manager";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./nixos/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.jdoe = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";    
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
       };
     };
+
+  in 
+  {
+  
+  nixosConfigurations = {
+    NixOSHTPC = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit system; };
+
+      modules = [
+      ./nixos/configuration.nix
+      ];
+    };
+  };
+
   };
 }
