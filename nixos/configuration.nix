2b4854d3b8dc3a3d8976e043a617cb9ee7546477
ChiropracticBender <noreply@digiwizard.ca>
2023-09-09 17:52:31 -0600

    disabling gnome apps

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,4 +142,22 @@
   # UNFREE SOFTWARE ALLOW
   nixpkgs.config.allowUnfree = true;
 
+  environment.gnome.excludePackages = (with pkgs; [
+  gnome-photos
+  gnome-tour
+]) ++ (with pkgs.gnome; [
+  cheese # webcam tool
+  gnome-music
+  gedit # text editor
+  epiphany # web browser
+  geary # email reader
+  evince # document viewer
+  gnome-characters
+  totem # video player
+  tali # poker game
+  iagno # go game
+  hitori # sudoku game
+  atomix # puzzle game
+]);
+
 }
