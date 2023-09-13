04ffdebdaeda3b11475c792d769e2f0a39849d4c
ChiropracticBender <noreply@digiwizard.ca>
2023-09-12 22:44:57 -0600

    reset kodi calling install.sh

--- tools/reset-kodi.sh
+++ tools/reset-kodi.sh
@@ -2,6 +2,11 @@
 
 echo "deleting kodi directory"
 sudo rm -r /home/htpc/.kodi
+
+## RUN INSTALL SH
+echo" Calling install.sh
+sudo ./install.sh
+
 echo "Setting default files inside .kodi for HTPC user"
 sudo cp -r /htpc/kodi /home/htpc/.kodi
 sudo reboot
