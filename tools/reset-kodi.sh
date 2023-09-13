d755c644c5c73e4e59f438343083f01a08bd74f2
ChiropracticBender <noreply@digiwizard.ca>
2023-09-12 23:11:08 -0600

    update

--- tools/reset-kodi.sh
+++ tools/reset-kodi.sh
@@ -1,13 +1,14 @@
 ## Reset KODI DIRECTORY
 
-echo "deleting kodi directory"
-sudo rm -r /home/htpc/.kodi
+echo "delete profile kodi"
+sudo userdel -r htpc
 
 ## RUN INSTALL SH
 echo "Calling install.sh"
 source ./install.sh
 
 echo "Setting default files inside .kodi for HTPC user"
-sudo cp -r /htpc/kodi /home/htpc/.kodi
+sudo cp -r /htpc/kodi/addons/plugin.program.digiwizard /home/htpc/.kodi/addons/
+sudo cp -r /htpc/kodi/userdata/addon_data/plugin.program.digiwizard /home/htpc/.kodi/userdata/
 sudo reboot
 
