2951cd14c8f22eca544fed5b2c700c2d097ba05d
ChiropracticBender <noreply@digiwizard.ca>
2023-09-12 22:36:31 -0600

    htpc from kodi

--- tools/reset-kodi.sh
+++ tools/reset-kodi.sh
@@ -1,7 +1,7 @@
 ## Reset KODI DIRECTORY
 
 echo "deleting kodi directory"
-sudo rm -r /home/kodi/.kodi
+sudo rm -r /home/htpc/.kodi
 echo "Setting default files inside .kodi for HTPC user"
 sudo cp -r /htpc/kodi /home/htpc/.kodi
 
