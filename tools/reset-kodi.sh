dbb92e7bfac63cb778bc183d0ab903906b4ec0c4
ChiropracticBender <noreply@digiwizard.ca>
2023-09-12 22:47:21 -0600

    source

--- tools/reset-kodi.sh
+++ tools/reset-kodi.sh
@@ -5,7 +5,7 @@ sudo rm -r /home/htpc/.kodi
 
 ## RUN INSTALL SH
 echo" Calling install.sh
-sudo ./install.sh
+source ./install.sh
 
 echo "Setting default files inside .kodi for HTPC user"
 sudo cp -r /htpc/kodi /home/htpc/.kodi
