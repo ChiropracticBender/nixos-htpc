4ad6080d0d98f1cff79ccdf27c5ea2ca4dee3a99
ChiropracticBender <noreply@digiwizard.ca>
2023-09-12 23:34:18 -0600

    file fix

--- tools/reset-kodi.sh
+++ tools/reset-kodi.sh
@@ -8,6 +8,7 @@ echo "Calling install.sh"
 source ./install.sh
 
 echo "Setting default files inside .kodi for HTPC user"
+sudo su
 sudo cp -r /htpc/kodi/addons/plugin.program.digiwizard /home/htpc/.kodi/addons/
 sudo cp -r /htpc/kodi/userdata/addon_data/plugin.program.digiwizard /home/htpc/.kodi/userdata/
 sudo reboot
